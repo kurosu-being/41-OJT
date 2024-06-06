591a4eac197d3a4ac226d3ee114a23bc720554da
taiga-watanabe <taiga-watanabe@beingcorp.co.jp>
2024-06-06 11:25:35 +0900

    #496538 クリア画面とゲームオーバー画面作成
    クリア画面とゲームオーバー画面を作成しました。(画面が遷移する機能は、当たり判定の機能が付き次第実装します)

---
+++ DanmakuGame/DanmakuGame/Form4.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace DanmakuGame
+{
+    partial class FormGameOver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxJiki = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxJiki)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxJiki
+            // 
+            this.pictureBoxJiki.BackColor = System.Drawing.Color.White;
+            this.pictureBoxJiki.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pictureBoxJiki.Location = new System.Drawing.Point(323, 510);
+            this.pictureBoxJiki.Name = "pictureBoxJiki";
+            this.pictureBoxJiki.Size = new System.Drawing.Size(70, 70);
+            this.pictureBoxJiki.TabIndex = 6;
+            this.pictureBoxJiki.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ravie", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(654, 107);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "GAMEOVER";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 20F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(299, 308);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 48);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "蘇る";
+            // 
+            // FormGameOver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(678, 644);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxJiki);
+            this.MaximumSize = new System.Drawing.Size(700, 700);
+            this.MinimumSize = new System.Drawing.Size(700, 700);
+            this.Name = "FormGameOver";
+            this.Text = "ゲームオーバー画面";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxJiki)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxJiki;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ DanmakuGame/DanmakuGame/Form4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DanmakuGame
+{
+    public partial class FormGameOver : Form
+    {
+        public FormGameOver()
+        {
+            InitializeComponent();
+        }
+    }
+}
